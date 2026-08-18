51af24295328449bde0ed0892ab7609668486091
Camilo Garcia <juan092005ca@gmail.com>
2026-08-18 12:47:41 -0500

    Creacion de CI y ajuste en clses testing

--- infrastructure/src/test/java/co/edu/uco/infraestructure/InfrastructureApplicationTests.java
+++ infrastructure/src/test/java/co/edu/uco/infraestructure/InfrastructureApplicationTests.java
@@ -1,13 +1,13 @@
 package co.edu.uco.infraestructure;
 
+import co.edu.uco.init.CrossWordApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CrossWordApplication.class)
 class InfrastructureApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
