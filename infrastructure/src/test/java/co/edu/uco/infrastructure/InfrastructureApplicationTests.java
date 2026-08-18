a1d03657c174b37e85d0d1cf29728b6fbf4708ed
Camilo Garcia <juan092005ca@gmail.com>
2026-08-18 12:36:06 -0500

    Ajuste en clase Testing infrastructure

--- infrastructure/src/test/java/co/edu/uco/infrastructure/InfrastructureApplicationTests.java
+++ infrastructure/src/test/java/co/edu/uco/infrastructure/InfrastructureApplicationTests.java
@@ -1,13 +1,13 @@
 package co.edu.uco.infrastructure;
 
+import co.edu.uco.infrastructure.init.CrossWordApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CrossWordApplication.class)
 class InfrastructureApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
